e25a7f29a34db5efb1e35e613ebee750f1701a5c
Laura Rodríguez <lauglitch@gmail..com>
2025-02-03 18:58:14 +0100

    Fix update

--- lauglitchLogic.js
+++ lauglitchLogic.js
@@ -284,7 +284,7 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('.barrido-izquierda, .barrido-derecha').forEach(elemento => {
             elemento.classList.add('active');
         });
-    }, 0); 
+    }, 100); // Pequeño retraso para asegurar que Blogspot haya cargado todo
 });
 
 ///////////// 3 - NAVIGATION
